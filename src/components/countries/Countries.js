52191bcc4e1cf1e52a17666b7b444da951f52df4
Shibbir Ahmed <shibbirahmedops@gmail.com>
2021-09-22 04:24:12 +0600

    data loading link added and color modified, logo spinning part modified

--- src/components/countries/Countries.js
+++ src/components/countries/Countries.js
@@ -12,6 +12,7 @@ const Countries = () => {
     return (
         <div id="total-data">
             <h1>We have data of total: {countries.length} countries!</h1>
+            <h3 id="siteInfo">Data is loaded from: <a href="https://restcountries.com/">https://restcountries.com/</a></h3>
             <div className="countryCards">
                 {
                     countries.map(country => <SingleCountry country={country} key={country.alpha3Code}></SingleCountry>)
