97b87ff5546092ed03439a9bc6b5ccd7c343780e
Shibbir Ahmed <shibbirahmedops@gmail.com>
2021-09-22 00:08:16 +0600

    first commit, header added

---
+++ src/components/countries/Countries.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Countries.css'
+
+const Countries = () => {
+    return (
+        <div>
+            <h1>Hi this is country part! Welcome</h1>
+        </div>
+    );
+};
+
+export default Countries;
---
+++ src/components/header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './header.css'
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div>
+                <h1>Welcome to Country Hub!</h1>
+            </div>
+
+            <div>
+                <input type="text" placeholder="Enter country name here!" id="header-input" />
+                <button id="header-button">Search</button>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
