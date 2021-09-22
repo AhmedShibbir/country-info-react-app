bdd2c5d540c4ae380c411273aad2af120f784b4e
Shibbir Ahmed <shibbirahmedops@gmail.com>
2021-09-22 13:09:48 +0600

    api url modified

--- src/components/countries/Countries.js
+++ src/components/countries/Countries.js
@@ -5,14 +5,14 @@ import SingleCountry from '../singleCountry/SingleCountry';
 const Countries = () => {
     const [countries, setCountries] = useState([]);
     useEffect(() => {
-        fetch("https://restcountries.eu/rest/v2/all")
+        fetch("https://raw.githubusercontent.com/ProgrammingHero1/rest-countries-data/main/coutries.JSON?fbclid=IwAR2I0FWfckuDaEBVBPHOKRhh4E4BQUyU1pZ9xMvsXkCNXD8wvcI-mq7tPUg")
         .then(res => res.json())
         .then(data => setCountries(data))
     },[])
     return (
         <div id="total-data">
             <h1>We have data of total: {countries.length} countries!</h1>
-            <h3 id="siteInfo">Data is loaded from: <a href="https://restcountries.eu/rest/v2/all">https://restcountries.eu/rest/v2/all</a></h3>
+            <h3 id="siteInfo">Data is loaded from: <a href="https://raw.githubusercontent.com/ProgrammingHero1/rest-countries-data/main/coutries.JSON?fbclid=IwAR2I0FWfckuDaEBVBPHOKRhh4E4BQUyU1pZ9xMvsXkCNXD8wvcI-mq7tPUg">https://raw.githubusercontent.com/ProgrammingHero1/rest-countries-data/main/coutries.JSON?fbclid=IwAR2I0FWfckuDaEBVBPHOKRhh4E4BQUyU1pZ9xMvsXkCNXD8wvcI-mq7tPUg</a></h3>
             <div className="countryCards">
                 {
                     countries.map(country => <SingleCountry country={country} key={country.alpha3Code}></SingleCountry>)
